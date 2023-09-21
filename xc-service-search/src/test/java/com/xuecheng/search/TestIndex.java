ff3e5b4392360be25e2a1f02674250cddcac069a
lkssess <946644535@qq.com>
2023-09-22 00:04:47 +0800

    9-21

--- xc-service-search/src/test/java/com/xuecheng/search/TestIndex.java
+++ xc-service-search/src/test/java/com/xuecheng/search/TestIndex.java
@@ -1,4 +1,163 @@
 package com.xuecheng.search;
 
+
+import org.elasticsearch.action.DocWriteResponse;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.IndicesClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContent;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.rest.RestStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class TestIndex {
+    @Autowired
+    RestHighLevelClient restHighLevelClient;
+
+
+    /**    测试删除索引
+     *
+     */
+    @Test
+    public void testDeleteIndex() throws IOException {
+        //创建索引请求对象
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("xc_course");
+        //删除索引
+       /*DeleteIndexResponse deleteIndexResponse = restHighLevelClient.indices().delete(deleteIndexRequest);
+        */
+            //操作索引的客户端
+            IndicesClient indices = restHighLevelClient.indices();
+            //删除上面指定的索引
+            DeleteIndexResponse deleteIndexResponse = indices.delete(deleteIndexRequest);
+        //删除后响应的结果
+            //拿到响应
+            boolean acknowledged = deleteIndexResponse.isAcknowledged();
+        System.out.println(acknowledged);
+    }
+
+    /**     测试创建索引库
+     * number_of_replicas      副本数量
+     * number_of_shards       分片数量
+     */
+    @Test
+    public void testCreateIndex() throws IOException {
+        //创建索引请求对象
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest("xc_course");
+            //对创建的索引对象设置参数
+            createIndexRequest.settings(Settings.builder().put("number_of_shards","1").put("number_of_replicas","0"));
+                //指定映射
+                createIndexRequest.mapping("doc","{\n" +
+                        "\t\"properties\": {\n" +
+                        "\t\t\"name\": {\n" +
+                        "\t\"type\": \"text\"\n" +
+                        "\t},\n" +
+                        "\t\"description\": {\n" +
+                        "\t\t\"type\": \"text\"\n" +
+                        "\t},\n" +
+                        "\t\"studymodel\": {\n" +
+                        "\t\t\"type\": \"keyword\"\n" +
+                        "\t}\n" +
+                        "\t}\n" +
+                        "}", XContentType.JSON);
+        //创建操作索引的客户端
+        IndicesClient indices = restHighLevelClient.indices();
+        //执行创建索引
+        CreateIndexResponse createIndexResponse = indices.create(createIndexRequest);
+        //拿到响应对象
+        boolean acknowledged = createIndexResponse.isAcknowledged();
+
+        System.out.println(acknowledged);
+    }
+
+    /**
+     * 添加文档
+     */
+    @Test
+    public void testAddDocument() throws IOException {
+        //添加的JSON数据
+        HashMap<String, Object> jsonMap = new HashMap<>();
+            jsonMap.put("name","springCloud实战");
+            jsonMap.put("description","Bootstrap是由Twitter推出的一个前台页面开发框架");
+            jsonMap.put("studymodel","201002");
+        //索引请求对象
+        IndexRequest indexRequest = new IndexRequest("xc_course", "doc");
+        //指定文档内容
+        IndexRequest source = indexRequest.source(jsonMap);
+        //客户端进行HTTP请求   拿到响应
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest);
+        //响应结果
+        DocWriteResponse.Result result = indexResponse.getResult();
+        System.out.println(result);
+    }
+    /**
+     * 查询文档
+     */
+    @Test
+    public void testGetDocument() throws IOException {
+        //索引请求对象
+        GetRequest indexRequest = new GetRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
+        //客户端查询
+        GetResponse getResponse= restHighLevelClient.get(indexRequest);
+            //得到文档内容
+            Map<String, Object> source = getResponse.getSourceAsMap();
+        //打印输出
+        System.out.println(source);
+    }
+
+    /**
+     * 更新文档
+     */
+    @Test
+    public void testUpdateDocument() throws IOException {
+        //索引请求对象
+        UpdateRequest updateRequest = new UpdateRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
+            //更新信息
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("name","updateInfo");
+            //存入请求
+            updateRequest.doc(map);
+        //客户端更新
+        UpdateResponse updateResponse = restHighLevelClient.update(updateRequest);
+        //获取更新状态
+        RestStatus status = updateResponse.status();
+        //打印输出
+        System.out.println(status);
+    }
+
+    /**
+     * 删除文档
+     */
+    @Test
+    public void testDeleteDocument() throws IOException {
+        //索引请求对象
+        DeleteRequest deleteRequest = new DeleteRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
+        //客户端删除信息
+        DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest);
+        //获取删除结果
+        RestStatus status = deleteResponse.status();
+        //打印输出
+        System.out.println(status);
+    }
 }
--- xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
+++ xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
@@ -12,14 +12,21 @@ import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.IndicesClient;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContent;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.rest.RestStatus;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,137 +34,174 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class TestIndex {
+public class TestSearch {
     @Autowired
     RestHighLevelClient restHighLevelClient;
 
-
-    /**    测试删除索引
-     *
-     */
-    @Test
-    public void testDeleteIndex() throws IOException {
-        //创建索引请求对象
-        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("xc_course");
-        //删除索引
-       /*DeleteIndexResponse deleteIndexResponse = restHighLevelClient.indices().delete(deleteIndexRequest);
-        */
-            //操作索引的客户端
-            IndicesClient indices = restHighLevelClient.indices();
-            //删除上面指定的索引
-            DeleteIndexResponse deleteIndexResponse = indices.delete(deleteIndexRequest);
-        //删除后响应的结果
-            //拿到响应
-            boolean acknowledged = deleteIndexResponse.isAcknowledged();
-        System.out.println(acknowledged);
-    }
-
-    /**     测试创建索引库
-     * number_of_replicas      副本数量
-     * number_of_shards       分片数量
-     */
-    @Test
-    public void testCreateIndex() throws IOException {
-        //创建索引请求对象
-        CreateIndexRequest createIndexRequest = new CreateIndexRequest("xc_course");
-            //对创建的索引对象设置参数
-            createIndexRequest.settings(Settings.builder().put("number_of_shards","1").put("number_of_replicas","0"));
-                //指定映射
-                createIndexRequest.mapping("doc","{\n" +
-                        "\t\"properties\": {\n" +
-                        "\t\t\"name\": {\n" +
-                        "\t\"type\": \"text\"\n" +
-                        "\t},\n" +
-                        "\t\"description\": {\n" +
-                        "\t\t\"type\": \"text\"\n" +
-                        "\t},\n" +
-                        "\t\"studymodel\": {\n" +
-                        "\t\t\"type\": \"keyword\"\n" +
-                        "\t}\n" +
-                        "\t}\n" +
-                        "}", XContentType.JSON);
-        //创建操作索引的客户端
-        IndicesClient indices = restHighLevelClient.indices();
-        //执行创建索引
-        CreateIndexResponse createIndexResponse = indices.create(createIndexRequest);
-        //拿到响应对象
-        boolean acknowledged = createIndexResponse.isAcknowledged();
-
-        System.out.println(acknowledged);
-    }
+    @Autowired
+    RestClient restClient;
 
     /**
-     * 添加文档
+     * 搜索全部记录
      */
     @Test
-    public void testAddDocument() throws IOException {
-        //添加的JSON数据
-        HashMap<String, Object> jsonMap = new HashMap<>();
-            jsonMap.put("name","springCloud实战");
-            jsonMap.put("description","Bootstrap是由Twitter推出的一个前台页面开发框架");
-            jsonMap.put("studymodel","201002");
-        //索引请求对象
-        IndexRequest indexRequest = new IndexRequest("xc_course", "doc");
-        //指定文档内容
-        IndexRequest source = indexRequest.source(jsonMap);
-        //客户端进行HTTP请求   拿到响应
-        IndexResponse indexResponse = restHighLevelClient.index(indexRequest);
-        //响应结果
-        DocWriteResponse.Result result = indexResponse.getResult();
-        System.out.println(result);
+    public void testSearchAll() throws IOException, ParseException {
+        //创建搜索请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+            //搜索对象指定类型
+            searchRequest.types("doc");
+        //创建搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            //搜索源设置搜索方式
+            searchSourceBuilder.query(QueryBuilders.matchAllQuery());          //QueryBuilders.matchAllQuery()-----搜索全部
+            //设置源文档字段过滤------------第一个结果集包括哪些字段，第二个结果集不包括哪些字段
+            searchSourceBuilder.fetchSource(new String[]{"name","studymodel","timestamp"},new String[]{});
+        //搜索请求对象设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest);
+            //获取匹配的搜索结果
+            SearchHits hits = searchResponse.getHits();
+            //匹配到的总记录数
+            long totalHits = hits.getTotalHits();
+            //匹配度高的文档
+            SearchHit[] searchHits = hits.getHits();
+
+        //日期格式化对象
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //遍历输出
+        for (SearchHit hit:searchHits) {
+            //文档主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //之前设置了源文档字段过滤集，所以搜索不到
+            String description = (String) sourceAsMap.get("description");
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+            //打印
+            System.out.println(name);
+            System.out.println(description);
+            System.out.println(studymodel);
+            System.out.println(timestamp);
+        }
     }
     /**
-     * 查询文档
+     * 分页搜索记录
      */
     @Test
-    public void testGetDocument() throws IOException {
-        //索引请求对象
-        GetRequest indexRequest = new GetRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
-        //客户端查询
-        GetResponse getResponse= restHighLevelClient.get(indexRequest);
-            //得到文档内容
-            Map<String, Object> source = getResponse.getSourceAsMap();
-        //打印输出
-        System.out.println(source);
+    public void testSearchPage() throws IOException, ParseException {
+        //创建搜索请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //搜索对象指定类型
+        searchRequest.types("doc");
+        //创建搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            //页码
+            int page = 1;
+            //每页记录数
+            int size = 1;
+            //计算记录起始下标
+            int from = (page - 1) * size;
+            //设置分页参数
+            searchSourceBuilder.from(from);
+            searchSourceBuilder.size(size);
+        //搜索源设置搜索方式
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());          //QueryBuilders.matchAllQuery()-----搜索全部
+        //设置源文档字段过滤------------第一个结果集包括哪些字段，第二个结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name","studymodel","timestamp"},new String[]{});
+        //搜索请求对象设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest);
+        //获取匹配的搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+
+        //日期格式化对象
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //遍历输出
+        for (SearchHit hit:searchHits) {
+            //文档主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //之前设置了源文档字段过滤集，所以搜索不到
+            String description = (String) sourceAsMap.get("description");
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+            //打印
+            System.out.println(name);
+            System.out.println(description);
+            System.out.println(studymodel);
+            System.out.println(timestamp);
+        }
+    }
+    /**
+     * 精确查询和根据ID查询
+     */
+    @Test
+    public void testSearchTermQuery() throws IOException, ParseException {
+        //创建搜索请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //搜索对象指定类型
+        searchRequest.types("doc");
+        //创建搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //搜索源设置搜索方式
+                    //termQuery精确查询
+                  //searchSourceBuilder.query(QueryBuilders.termQuery("name","spring"));          //termQuery-----精确查询
+                    // searchSourceBuilder.query(QueryBuilders.termQuery("name","spring开发"));     //因为是整体精确查询，没有相匹配的值，搜不出来
+                    //设置源文档字段过滤------------第一个结果集包括哪些字段，第二个结果集不包括哪些字段
+                            //根据ID查询
+                                //设置主键
+                                String[] ids = new String[]{"1","2"};
+                                searchSourceBuilder.query(QueryBuilders.termsQuery("_id",ids));          //注意：这里的QueryBuilders.termsQuery 是termsQuery
+        searchSourceBuilder.fetchSource(new String[]{"name","studymodel","timestamp"},new String[]{});
+        //搜索请求对象设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest);
+        //获取匹配的搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+
+        //日期格式化对象
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //遍历输出
+        for (SearchHit hit:searchHits) {
+            //文档主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //之前设置了源文档字段过滤集，所以搜索不到
+            String description = (String) sourceAsMap.get("description");
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+            //打印
+            System.out.println(name);
+            System.out.println(description);
+            System.out.println(studymodel);
+            System.out.println(timestamp);
+        }
     }
 
-    /**
-     * 更新文档
-     */
-    @Test
-    public void testUpdateDocument() throws IOException {
-        //索引请求对象
-        UpdateRequest updateRequest = new UpdateRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
-            //更新信息
-            HashMap<String, Object> map = new HashMap<>();
-            map.put("name","updateInfo");
-            //存入请求
-            updateRequest.doc(map);
-        //客户端更新
-        UpdateResponse updateResponse = restHighLevelClient.update(updateRequest);
-        //获取更新状态
-        RestStatus status = updateResponse.status();
-        //打印输出
-        System.out.println(status);
-    }
-
-    /**
-     * 删除文档
-     */
-    @Test
-    public void testDeleteDocument() throws IOException {
-        //索引请求对象
-        DeleteRequest deleteRequest = new DeleteRequest("xc_course", "doc","VRLLnIoBrB8-xp-foVZu");
-        //客户端删除信息
-        DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest);
-        //获取删除结果
-        RestStatus status = deleteResponse.status();
-        //打印输出
-        System.out.println(status);
-    }
 }
